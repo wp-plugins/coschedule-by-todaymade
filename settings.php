bdfd439538c59f8f3ec8655fbd73e5f684d56512
todaymade <todaymade@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-06 23:38:58 +0000

    Bump version to 1.8.2
    git-svn-id: https://plugins.svn.wordpress.org/coschedule-by-todaymade/tags/1.8.2@800112 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ settings.php
@@ -0,0 +1,120 @@
+<script type='text/javascript'>
+    var tm_cos_token = "<?php echo get_option('tm_coschedule_token'); ?>";
+</script>
+<!--[if lt IE 9]>
+  <style>
+    .cos-settings-wrap .ie-only {
+        display:block !important;
+    }
+    .cos-settings-wrap .not-ie {
+        display: none !important;
+    }
+  </style>
+<![endif]-->
+
+<div class="coschedule">
+    <div class="cos-settings-wrap">
+
+        <div class="cos-header">
+            <a href="http://coschedule.com" target="_blank" class="cos-logo-full pull-left" style="margin-bottom:20px;"></a>
+            <ul class="nav navbar-tabs pull-right">
+                <li>
+                    <a href="http://app.coschedule.com" target="_blank" title="My Account" class="text-center">
+                        <span aria-hidden="true" class="icon-user"></span>
+                    </a>
+                </li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="cos-hero">
+            <!-- Settings Form -->
+            <form id="cs-setting-form" method="post" action="options.php">
+                <table class="cos-form-table">
+                    <tr valign="middle">
+                        <td>
+                            <div id="tm_deactivate" style="display:none;">
+                                <img src="<?=$this->assets.'/img/vector-editorial-calendar-ready.png';?>" class="calendar-image" alt="Your CoSchedule Calendar Is Ready"/>
+                                <h3>Your calendar is ready!</h3>
+                                <a href="admin.php?page=tm_coschedule_calendar" class="btn btn-orange btn-large">View Your Calendar</a><br/>
+                                <button class="btn-disconnect" type="button" id="tm_deactivate_button">Disconnect</button>
+                            </div>
+                            <div id="tm_activate" style="display:none;">
+                                <label for="tm_coschedule_token"><h3>Connect to CoSchedule</h3></label>
+                                <div id="tm_coschedule_alert" class="alert" style="display:none;"></div>
+                                <form name="connect-blog">
+                                    <label class="form-label ie-only text-left">Email Address</label>
+                                    <input class="input-jumbo input-block-level" type="text" name="tm_coschedule_email" id="tm_coschedule_email" placeholder="email"><br>
+                                    <label class="form-label ie-only text-left">Password</label>
+                                    <input class="input-jumbo input-block-level" type="password" name="tm_coschedule_password" id="tm_coschedule_password" placeholder="password"><br>
+                                    <button type="submit" class="btn btn-blue btn-jumbo btn-block btn-caps" id="tm_activate_button">Connect</button>
+                                    <p>Activate your plugin by connecting to CoSchedule. If you don’t have an account, you can <a href="http://app.coschedule.com/#/register" target="_blank">register here</a>.</p>
+                                    <input type="hidden" id="" value="">
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+
+            <div class="cos-hero-bottom">
+                <div class="pull-left">
+                    Version 1.8.2
+                </div>
+                <div class="pull-right">
+                    <a href="#" id="tm_debug_toggle">Debug Information</a>
+                </div>
+            </div>
+
+            <div class="cos-hero-debug-info" id="tm_debug_info" style="display:none;">
+                <p>Having problems with the plugin? Check out our <a href="http://coschedule.com/help" target="_blank">FAQ</a> documentation. You can also <a href="mailto:support@coschedule.com">drop us a line</a> including the following details and we'll do what we can.</p>
+                <textarea>URL: <?php echo get_option('siteurl'); ?> &#10;Timezone: <?php echo get_option('timezone_string').' -- GMT Offset: '.get_option('gmt_offset'); ?> &#10;CoS Version: 1.8.2 &#10;CoS Build: 12 &#10;CoS Token: <?php echo get_option('tm_coschedule_token'); ?> &#10;CoS ID: <?php echo get_option('tm_coschedule_id'); ?>&#10;CoS App: https://app.coschedule.com &#10;CoS Assets: https://d27i93e1y9m4f5.cloudfront.net &#10;CoS API: https://api.coschedule.com &#10;WP Version: <?php echo get_bloginfo('version'); ?> &#10;PHP Version: <?php echo phpversion(); ?> &#10;User String: <?php echo $_SERVER['HTTP_USER_AGENT']; ?> &#10;Active Theme: <?php $my_theme = wp_get_theme(); echo $my_theme->Name . " is version " . $my_theme->Version; ?> &#10;Plugins: &#10;<?php
+                foreach (get_plugins() as $key => $plugin) {
+                    $isactive = "";
+                    if (is_plugin_active($key)) {
+                        $isactive = "(active)";
+                    }
+                    echo $plugin['Name'].' '.$plugin['Version'].' '.$isactive."\n";
+                }
+                ?></textarea>
+            </div>
+        </div>
+
+        <div class="cos-content">
+            <h3 class="orange">What is CoSchedule?</h3>
+            <p class="lead">
+                CoSchedule is an editorial and social media calendar for publishers. It integrates directly with your WordPress blogs and social media profiles to simplify your content marketing workflow and make content scheduling easier on you and your team.
+            </p>
+            <p class="lead">
+                It is made by Todaymade.
+            </p>
+            <div class="text-center">
+                <a href="http://todaymade.com" target="_blank" class="todaymade-logo"></a>
+            </div>
+        </div>
+
+        <hr style="margin-top:40px;"/>
+
+        <div style="float: right; margin-top:20px;">
+            <iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.1368146021.html#_=1369774539012&amp;id=twitter-widget-1&amp;lang=en&amp;screen_name=todaymade&amp;show_count=false&amp;show_screen_name=true&amp;size=l" class="twitter-follow-button twitter-follow-button" title="Twitter Follow Button" data-twttr-rendered="true" style="width: 163px; height: 28px;"></iframe>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+            <iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.1368146021.html#_=1369774539012&amp;id=twitter-widget-1&amp;lang=en&amp;screen_name=coschedule&amp;show_count=false&amp;show_screen_name=true&amp;size=l" class="twitter-follow-button twitter-follow-button" title="Twitter Follow Button" data-twttr-rendered="true" style="width: 163px; height: 28px;"></iframe>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+            <iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/tweet_button.1368146021.html#_=1369774539009&amp;count=horizontal&amp;id=twitter-widget-0&amp;lang=en&amp;original_referer=http%3A%2F%2Fcoschedule.com%2Faccount&amp;related=coschedule&amp;size=l&amp;text=Check%20this%20awesome%20new%20%23wordpress%20tool%20-%20Organize%20%26%20Automate%20Your%20WordPress%20Content&amp;url=http%3A%2F%2Fwww.coschedule.com%2F&amp;via=coschedule" class="twitter-share-button twitter-count-horizontal" title="Twitter Tweet Button" data-twttr-rendered="true" style="width: 138px; height: 28px;"></iframe>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
+
+        <div style="clear:both;"></div>
+
+        <hr style="margin-top:20px;"/>
+
+        <a href="http://coschedule.com/help" target="_blank">Help</a>
+        &nbsp;&bull;&nbsp; <a href="mailto:support@coschedule.com">Support/Feedback</a>
+        <div style="float: right;">
+            CoSchedule by <a href="http://todaymade.com" target="_blank">Todaymade</a>
+        </div>
+
+    </div>
+</div>
---
+++ tm-scheduler.php
@@ -0,0 +1,593 @@
+<?php
+/*
+Plugin Name: CoSchedule by Todaymade
+Description: Schedule social media messages alongside your blog posts in WordPress, and then view them on a Google Calendar interface. <a href="http://app.coschedule.com" target="_blank">Account Settings</a>
+Version: 1.8.2
+Author: Todaymade
+Author URI: http://todaymade.com/
+Plugin URI: http://coschedule.com/
+*/
+
+// Check for existing class
+if ( ! class_exists( 'tm_coschedule' ) ) {
+
+	// Include Http Class
+	if(!class_exists( 'WP_Http' )) {
+		include_once( ABSPATH . WPINC. '/class-http.php' );
+	}
+
+	/**
+	 * Main Class
+	 */
+	class tm_coschedule  {
+		private $api = "https://api.coschedule.com";
+		private $assets = "https://d27i93e1y9m4f5.cloudfront.net";
+		private $version = "1.8.2";
+		private $build = 12;
+		private $connected = false;
+		private $token = false;
+
+		/**
+		 * Class constructor: initializes class variables and adds actions and filters.
+		 */
+		public function __construct() {
+			$this->tm_coschedule();
+		}
+
+		public function tm_coschedule() {
+			register_activation_hook( __FILE__, array($this, 'activation' ) );
+			register_deactivation_hook( __FILE__, array($this, 'deactivation' ) );
+
+			// Load token
+			$this->token = get_option('tm_coschedule_token');
+
+			// Check if connected to api
+			if (isset($this->token) && !empty($this->token)) {
+				$this->connected = true;
+			}
+
+			// Register global hooks
+			$this->register_global_hooks();
+
+			// Register admin only hooks
+			if(is_admin()) {
+				$this->register_admin_hooks();
+			}
+		}
+
+		/**
+		 * Print the contents of an array
+		 */
+		public function debug($array) {
+			echo '<pre>';
+			print_r($array);
+			echo '</pre>';
+		}
+
+		/**
+		 * Handles activation tasks, such as registering the uninstall hook.
+		 */
+		public function activation() {
+			register_uninstall_hook( __FILE__, array($this, 'uninstall'));
+		}
+
+		/**
+		 * Handles deactivation tasks, such as deleting plugin options.
+		 */
+		public function deactivation() {
+			delete_option('tm_coschedule_token');
+			delete_option('tm_coschedule_id');
+		}
+
+		/**
+		 * Handles uninstallation tasks, such as deleting plugin options.
+		 */
+		public function uninstall() {
+			delete_option('tm_coschedule_token');
+			delete_option('tm_coschedule_id');
+		}
+
+		/**
+		 * Registers global hooks, these are added to both the admin and front-end.
+		 */
+		public function register_global_hooks() {
+			// Called whenever a post is created/updated/deleted
+			add_action( 'save_post', array($this, "save_post_callback"));
+			add_action( 'delete_post', array($this, "delete_post_callback"));
+
+			// Called whenever a post is created/updated/deleted
+			add_action( 'create_category', array($this, "save_category_callback"));
+			add_action( 'edit_category', array($this, "save_category_callback"));
+			add_action( 'delete_category', array($this, "delete_category_callback"));
+
+			// Called whenever a user/author is created/updated/deleted
+			add_action( 'user_register', array($this, "save_user_callback"));
+			add_action( 'profile_update', array($this, "save_user_callback"));
+			add_action( 'delete_user', array($this, "delete_user_callback"));
+
+			// Edit Flow Fix
+			add_filter('wp_insert_post_data', array($this, 'fix_custom_status_timestamp_before'), 1);
+			add_filter('wp_insert_post_data', array($this, 'fix_custom_status_timestamp_after'), 20);
+		}
+
+		/**
+		 * Registers admin only hooks.
+		 */
+		public function register_admin_hooks() {
+			// Add meta box setup actions to post edit screen
+			add_action('load-post.php', array($this, "meta_box_action"));
+			add_action('load-post-new.php', array($this, "meta_box_action"));
+
+			// Add meta box css and script to post edit screen
+			add_action('load-post.php', array($this, "meta_box_scripts"));
+			add_action('load-post-new.php', array($this, "meta_box_scripts"));
+
+			// Ajax: Get blog info
+			add_action('wp_ajax_tm_aj_get_bloginfo', array($this, 'tm_aj_get_bloginfo'));
+			add_action('wp_ajax_nopriv_tm_aj_get_bloginfo', array($this, 'tm_aj_get_bloginfo'));
+
+			// Ajax: Get full post with permalink
+			add_action('wp_ajax_tm_aj_get_full_post', array($this, 'tm_aj_get_full_post'));
+			add_action('wp_ajax_nopriv_tm_aj_get_full_post', array($this, 'tm_aj_get_full_post'));
+
+			// Ajax: Set token
+			add_action('wp_ajax_tm_aj_set_token', array($this, 'tm_aj_set_token'));
+
+			// Ajax: Get function
+			add_action('wp_ajax_tm_aj_function', array($this, 'tm_aj_function'));
+			add_action('wp_ajax_nopriv_tm_aj_function', array($this, 'tm_aj_function'));
+
+			// Ajax: Deactivation
+			add_action('wp_ajax_tm_aj_deactivation', array($this, 'tm_aj_deactivation'));
+			add_action('wp_ajax_nopriv_tm_aj_deactivation', array($this, 'tm_aj_deactivation'));
+
+			// Add Sidebar Settings Link
+			add_action('admin_menu', array($this, 'add_menu'));
+
+			// Add settings link to plugins listing page
+			add_filter('plugin_action_links', array($this, 'plugin_settings_link'), 2, 2);
+		}
+
+		/**
+		* Add calendar and settings link to the admin menu
+		*/
+		public function add_menu() {
+			// Settings Page
+			$settings_1 = add_options_page('CoSchedule Settings', 'CoSchedule', 'manage_options', 'tm_coschedule', array($this, 'plugin_settings_page'));
+			// Enqueue scripts for settings
+			add_action('admin_print_styles-' . $settings_1, array($this, 'plugin_settings_scripts'));
+
+			// Swith main nav link between settings and the calendar depending on if they are connected
+			if ($this->connected) {
+				add_menu_page('CoSchedule Calendar', 'Calendar', 'edit_posts', 'tm_coschedule_calendar', array($this, 'plugin_calendar_page'), $this->assets.'/plugin/icon.png', '50.505' );
+			} else {
+				$settings_2 = add_menu_page('CoSchedule Calendar', 'Calendar', 'edit_posts', 'tm_coschedule_calendar', array($this, 'plugin_settings_page'), $this->assets.'/plugin/icon.png', '50.505' );
+				// Enqueue scripts for settings
+				add_action('admin_print_styles-' . $settings_2, array($this, 'plugin_settings_scripts'));
+			}
+		}
+
+		/**
+		 * Admin: Add settings link to plugin management page
+		 */
+		public function plugin_settings_link($actions, $file) {
+			if(false !== strpos($file, 'tm-scheduler')) {
+				$actions['settings'] = '<a href="options-general.php?page=tm_coschedule">Settings</a>';
+			}
+			return $actions;
+		}
+
+		/**
+		* Setttings page menu callback
+		*/
+		public function plugin_settings_page() {
+			if(!current_user_can('manage_options')) {
+				wp_die(__('You do not have sufficient permissions to access this page.'));
+			}
+			include(sprintf("%s/settings.php", dirname(__FILE__)));
+		}
+
+		/**
+		* Settings page scripts
+		*/
+		public function plugin_settings_scripts() {
+			$cache_bust = $this->get_cache_bust();
+
+			wp_enqueue_style('cos_css', $this->assets.'/css/wordpress_plugin.min.css?cb='.$cache_bust);
+			wp_enqueue_script('cos_js_config', $this->assets.'/js/config.js?cb='.$cache_bust, false, null, true);
+			wp_enqueue_script('cos_js_plugin', $this->assets.'/js/settings.js?cb='.$cache_bust, false, null, true);
+		}
+
+		/**
+		* Calendar page menu callback
+		*/
+		public function plugin_calendar_page() {
+			if(!current_user_can('edit_posts')) {
+				wp_die(__('You do not have sufficient permissions to access this page.'));
+			}
+			include(sprintf("%s/calendar.php", dirname(__FILE__)));
+		}
+
+		/**
+		 * Registers the javascript variables for the page
+		 */
+		public function plugin_js_variables() {
+			return array(
+				'build' => $this->build,
+				'version' => $this->version,
+				'post_id' => get_the_ID(),
+				'token' => get_option('tm_coschedule_token'),
+				'post' => $this->get_full_post(get_the_ID())
+			);
+		}
+
+		/**
+		 * Adds action to insert a meta box
+		 */
+		public function meta_box_action() {
+			add_action('add_meta_boxes', array($this, "meta_box_setup"));
+		}
+
+		/**
+		 * Enqueue the css and js for the metabox
+		 */
+		public function meta_box_scripts() {
+			$cache_bust = $this->get_cache_bust();
+			$post_type = $this->get_current_post_type();
+
+			// Only load our scripts on post
+			if($post_type === 'post') {
+				wp_enqueue_style('cos_css', $this->assets.'/css/wordpress_plugin.css?cb='.$cache_bust);
+				wp_enqueue_script('cos_js_config', $this->assets.'/js/config.js?cb='.$cache_bust, false, null, true);
+				wp_enqueue_script('cos_js_plugin', $this->assets.'/js/plugin.js?cb='.$cache_bust, false, null, true);
+			}
+		}
+
+		/**
+		 * Sets up the meta box to be inserted
+		 */
+		public function meta_box_setup() {
+			add_meta_box(
+				'tm-scheduler',						    // Unique ID
+				'CoSchedule',							// Title
+				array(&$this, 'meta_box_insert'),		// Callback function
+				'post',									// Admin page (or post type)
+				'normal',								// Context
+				'default'								// Priority
+			);
+		}
+
+		/**
+		 * Inserts the meta box
+		 */
+		public function meta_box_insert() {
+			echo '
+			<div id="tm-coschedule-outlet"></div>
+			<script>
+				var tm_coschedule_variables = '. json_encode($this->plugin_js_variables()) .';
+			</script>
+			';
+		}
+
+		/**
+		 * Ajax: Secure using token
+		 */
+		public function valid_token($token) {
+			$validate = "";
+			if (isset($token) && !empty($token)) {
+				if ($this->connected) {
+					if ($this->token === $token) {
+						$validate = true;
+					} else {
+						$validate = "Invalid token";
+					}
+				} else {
+					$validate = "Not connected to api";
+				}
+			} else {
+				$validate = "Token required";
+			}
+
+			if ($validate === true) {
+				return true;
+			} else {
+				echo json_encode(array("error"=>$validate));
+				die();
+			}
+		}
+
+		/**
+		 * Ajax: Return blog info
+		 */
+		public function tm_aj_get_bloginfo() {
+			echo json_encode($this->array_decode_entities(array(
+				"name"=>get_bloginfo("name"),
+				"description"=>get_bloginfo("description"),
+				"wpurl"=>get_bloginfo("wpurl"),
+				"url"=>get_bloginfo("url"),
+				"version"=>get_bloginfo("version"),
+				"language"=>get_bloginfo("language"),
+				"pingback_url"=>get_bloginfo("pingback_url"),
+				"rss2_url"=>get_bloginfo("rss2_url"),
+				"timezone_string"=>get_option("timezone_string"),
+				"gmt_offset"=>get_option("gmt_offset"),
+				"plugin_version"=>$this->version,
+				"plugin_build"=>$this->build
+				)));
+			die();
+		}
+
+		/**
+		 * Ajax: Return full post with permalink
+		 */
+		public function tm_aj_get_full_post() {
+			echo json_encode($this->get_full_post($_GET['post_id']));
+			die();
+		}
+
+		/**
+		 * Ajax: Set token
+		 */
+		public function tm_aj_set_token() {
+			update_option('tm_coschedule_token', $_POST['token']);
+			update_option('tm_coschedule_id', $_POST['id']);
+			echo $_POST['token'];
+			die();
+		}
+
+		/**
+		 * Ajax: Get function
+		 */
+		public function tm_aj_function() {
+			// Validate call
+			$this->valid_token($_GET['token']);
+
+			// Save args
+			$args = $_GET;
+
+			// Remove action name
+			unset($args['action']);
+
+			// Remove token
+			unset($args['token']);
+
+			// Save and remove function name
+			$func = $args['call'];
+			unset($args['call']);
+
+			// Call public or private Function
+			if (isset($args['private'])) {
+				unset($args['private']);
+				$out = call_user_func_array(array($this, $func), $args);
+			} else {
+				$out = call_user_func_array($func, $args);
+			}
+
+			if (is_array($out)) {
+				echo json_encode($out);
+			} else {
+				// Check for errors
+				if (is_wp_error($out) ) {
+					echo $out->get_error_message();
+				} else {
+					echo $out;
+				}
+			}
+			die();
+		}
+
+		/**
+		 * AJAX: Handles deactivation task
+		 */
+		public function tm_aj_deactivation() {
+			// Validate call
+			$this->valid_token($_GET['token']);
+
+			delete_option('tm_coschedule_token');
+			delete_option('tm_coschedule_id');
+			die();
+		}
+
+		/**
+		 * Get the post by id, with permalink
+		 */
+		public function get_full_post($post_id) {
+			$post = get_post($post_id, "ARRAY_A");
+			$post['permalink'] = get_permalink($post_id);
+			return $post;
+		}
+
+		/**
+		 * Get posts with permalinks and categories
+		 */
+		public function get_posts_with_categories($args) {
+			// Load posts
+			$out = call_user_func_array('get_posts', $args);
+
+			$posts = array();
+			foreach ($out as $post) {
+				$post = $this->get_full_post($post->ID);
+				if (!is_null($post['post_category'])) {
+					$post['post_category'] = implode($post['post_category'], ',');
+				} else {
+					$post['post_category'] = "";
+				}
+				array_push($posts, $post);
+			}
+			return $posts;
+		}
+
+		/**
+		 * Callback for when a post is created or updated
+		 */
+		public function save_post_callback($post_id) {
+			// Verify post is not a revision
+			if ($this->connected && !wp_is_post_revision($post_id)) {
+				// Load post
+				$post = $this->get_full_post($post_id);
+
+				// Send to API
+				$this->api_post('/hook/wordpress_posts/save?_wordpress_key='.$this->token, $post);
+			}
+		}
+
+		/**
+		 * Callback for when a post is deleted
+		 */
+		public function delete_post_callback($post_id) {
+			// Verify post is not a revision
+			if ($this->connected && !wp_is_post_revision($post_id)){
+				// Send to API
+				$this->api_post('/hook/wordpress_posts/delete?_wordpress_key='. $this->token, array('post_id'=>$post_id));
+			}
+		}
+
+		/**
+		 * Callback for when a category is created or updated
+		 */
+		public function save_category_callback($category_id) {
+			if ($this->connected) {
+				$category = get_category($category_id, "ARRAY_A");
+				$this->api_post('/hook/wordpress_categories/save?_wordpress_key='.$this->token, $category);
+			}
+		}
+
+		/**
+		 * Callback for when a category is deleted
+		 */
+		public function delete_category_callback($category_id) {
+			if ($this->connected) {
+				$resp=$this->api_post('/hook/wordpress_categories/delete?_wordpress_key='. $this->token, array('cat_id'=>$category_id));
+			}
+		}
+
+		/**
+		 * Callback for when a user is created or updated
+		 */
+		public function save_user_callback($user_id) {
+			if ($this->connected) {
+				$user = get_userdata($user_id);
+				$can_edit = $user->has_cap('edit_posts');
+				if ($can_edit) {
+					$this->api_post('/hook/wordpress_authors/save?_wordpress_key='. $this->token, (array)$user->data);
+				} else {
+					// Remove
+					$this->delete_user_callback($user_id);
+				}
+			}
+		}
+
+		/**
+		 * Callback for when a user is deleted
+		 */
+		public function delete_user_callback($user_id) {
+			if ($this->connected) {
+				$this->api_post('/hook/wordpress_authors/delete?_wordpress_key='. $this->token , array('user_id'=>$user_id));
+			}
+		}
+
+		/**
+		 * Post data to a url on the api
+		 * Returns: Result of call
+		 */
+		public function api_post($url, $body) {
+			$request = new WP_Http;
+			return $request->request($this->api.$url, array('method' => 'POST', 'body' => $this->array_decode_entities($body)));
+		}
+
+		/**
+		 * Get data from a url on the api
+		 * Returns: Result of call
+		 */
+		public function api_get($url) {
+			$request = new WP_Http;
+			return $request->request($this->api.$url);
+		}
+
+		/**
+		 * Get cache bust number from assets
+		 * Returns: Number from text file
+		 */
+		public function get_cache_bust() {
+			$location = 'https://d27i93e1y9m4f5.cloudfront.net/plugin/cache_bust.txt';
+			$request = new WP_Http;
+			$result = $request->request($location);
+			if (is_array($result) && isset($result['body'])) {
+				return $result['body'];
+			}
+			return '0';
+		}
+
+		/**
+		 * Given an array it html_entity_decodes every element of the array that is a string.
+		 */
+		public function array_decode_entities($array){
+			$new_array = array();
+
+			foreach ($array as $key => $string) {
+				if(is_string($string)) {
+					$new_array[$key] = html_entity_decode($string, ENT_QUOTES);
+				} else {
+					$new_array[$key] = $string;
+				}
+			}
+
+			return $new_array;
+		}
+
+		/**
+		* Edit Flow Fix: Runs before the edit flow function that modifies the post_date_gmt
+		*/
+		public function fix_custom_status_timestamp_before($data) {
+			// Save post_date_gmt for later
+			global $cos_cached_post_date_gmt;
+			if (isset($data['post_date_gmt']) && !empty($data['post_date_gmt'])) {
+				$cos_cached_post_date_gmt = $data['post_date_gmt'];
+			}
+
+			return $data;
+		}
+
+		/**
+		* Edit Flow Fix: Runs after the edit flow function that modifies the post_date_gmt
+		*/
+		public function fix_custom_status_timestamp_after($data) {
+			global $cos_cached_post_date_gmt;
+			if (isset($cos_cached_post_date_gmt) && !empty($cos_cached_post_date_gmt)) {
+				$data['post_date_gmt'] = $cos_cached_post_date_gmt;
+			}
+			return $data;
+		}
+
+		/**
+		* Get's the current post's post_type.
+		*/
+		public function get_current_post_type() {
+			global $post, $typenow, $current_screen;
+
+			//we have a post so we can just get the post type from that
+			if ($post && $post->post_type)
+				return $post->post_type;
+
+			//check the global $typenow - set in admin.php
+			elseif($typenow)
+				return $typenow;
+
+			//check the global $current_screen object - set in sceen.php
+			elseif($current_screen && $current_screen->post_type)
+				return $current_screen->post_type;
+
+			//lastly check the post_type querystring
+			elseif(isset( $_REQUEST['post_type']))
+				return sanitize_key( $_REQUEST['post_type']);
+
+			//we do not know the post type!
+			return null;
+		}
+	} // End tm_coschedule class
+
+	// Init Class
+	new tm_coschedule();
+}
+
+?>
