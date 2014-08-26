6dafd926beacbd6f16acfc33b95db78a642fbbdb
todaymade <todaymade@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-26 16:02:52 +0000

    Bump to version 2.1.3
    git-svn-id: https://plugins.svn.wordpress.org/coschedule-by-todaymade/tags/2.1.3@973403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,31 @@
+<?php
+if (get_option('tm_coschedule_token')) {
+    if (current_user_can('edit_posts')) {
+?>
+    <iframe id="CoSiFrame" frameborder=0 border=0 src="https://dgp49e2avkx9v.cloudfront.net/#/<?php echo get_option('tm_coschedule_token'); ?>/<?php echo get_current_user_id(); ?>/0/help" width="100%"></iframe>
+
+    <script>
+        jQuery(document).ready(function($) {
+            $('.update-nag').remove();
+            $('#wpfooter').remove();
+            $('#wpwrap #footer').remove();
+            $('#wpbody-content').css('paddingBottom', 0);
+            var resize = function() {
+                var p = $('#wpbody').height();
+                $('#CoSiFrame').height(p);
+            }
+
+            resize();
+            $(window).resize(function() {
+                resize();
+            });
+        });
+    </script>
+<?php
+    } else {
+        include('_access-denied.php');
+    }
+} else {
+    include('_missing-token.php');
+}
+?>
